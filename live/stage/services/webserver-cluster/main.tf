b0f65f81ef6c4296d953e30e031477c9ae6d4472
valkyraycho <razor0424@gmail.com>
2025-12-30 23:14:30 +0800

    [feat] use ssh as source

--- live/stage/services/webserver-cluster/main.tf
+++ live/stage/services/webserver-cluster/main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 }
 
 module "webserver-cluster" {
-  source = "../../../modules/services/webserver-cluster"
+  source = "git@github.com:valkyraycho/terraform-practice.git//modules/services/webserver-cluster?ref=v0.0.1"
 
   cluster_name           = "webservers-stage"
   db_remote_state_bucket = "terraform-up-and-running-state-valkyray-187457215304"
