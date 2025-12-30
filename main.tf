1aa5c78135b282ac203723736a0207bd268750ba
valkyraycho <razor0424@gmail.com>
2025-12-30 10:15:03 +0800

    [feat] add output and vars

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "example" {
   user_data = <<-EOF
                 #!/bin/bash
                 echo "Hello, World" > index.html
-                nohup python3 -m http.server 8080 &
+                nohup python3 -m http.server ${var.server_port} &
             EOF
 
   user_data_replace_on_change = true
@@ -29,6 +29,17 @@ resource "aws_vpc_security_group_ingress_rule" "example" {
   security_group_id = aws_security_group.instance.id
 
   cidr_ipv4 = "0.0.0.0/0"
-  from_port = 8080
-  to_port = 8080
+  from_port = var.server_port
+  to_port = var.server_port
+}
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type = number
+  default = 8080
+}
+
+output "public_ip" {
+  value = aws_instance.example.public_ip
+  description = "The public IP address of the web server"
 }
